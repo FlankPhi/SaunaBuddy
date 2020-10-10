19b8fa008ac0d71e5e8d4b4f211213b87d1c6d4f
MikalB <mikal.berge@outlook.com>
2020-10-10 12:57:27 +0200

    added get curret time

--- Database/db_interface.js
+++ Database/db_interface.js
@@ -54,7 +54,7 @@ const get_time = function(res){
     let pg_string = "SELECT  now();";
     pgPool.query(pg_string).then(resu => {
         console.log("Retuning current time");
-        res.status(200).json({"time": resu.rows[0], "succsess": true});
+        res.status(200).json({"time": resu.rows[0].now, "succsess": true});
     })
         .catch(err => {
             console.log("error retuning current time");
